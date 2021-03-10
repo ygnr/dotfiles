5bc4c290240b62e8384e425868688815bc9d4478
Gokulnath Reddy <yrgokulnath@kpmg.com.au>
2021-03-10 23:11:58 +1100

    add git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -9,14 +9,15 @@ fi
 # The rest of my fun git aliases
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
+alias gp='git push origin'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
 
-alias gc='git commit'
+alias gc='git commit -m'
 alias gca='git commit -a'
 alias gco='git checkout'
+alias gcob='git checkout -b'
 alias gcb='git copy-branch-name'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,3 +9,5 @@ alias edit='code' # use vscode
 alias wcode='cd ~/Work/Code' # Work code directory
 
 alias pcode='cd ~/Code' # Personal code directory
+
+alias home='cd ~/' # Personal code directory
