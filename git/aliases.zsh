2e285750651a0ea99843780df063eb2ee7585be2
Gokulnath Reddy <yrgokulnath@kpmg.com.au>
2021-03-14 17:20:54 +1100

    add alais

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -10,6 +10,7 @@ fi
 alias gl='git pull --prune'
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias gp='git push origin'
+alias gf='git fetch --all'
 
 # Remove `+` and `-` from start of diff lines; just rely upon color.
 alias gd='git diff --color | sed "s/^\([^-+ ]*\)[-+ ]/\\1/" | less -r'
